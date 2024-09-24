519d310af8e973df3374e52a78810e4cb328373a
Maria Zonfrillo <140718892+maria-zonfrillo@users.noreply.github.com>
2024-09-24 17:07:11 -0400

    Update and rename example.sql to pediatric-patient-roster.sql

---
+++ pediatric-patient-roster.sql
@@ -0,0 +1,23 @@
+WITH last_visit AS (
+  SELECT patient_id,
+         id AS visit_id,
+         date AS visit_date,
+         ROW_NUMBER() OVER (PARTIION BY patient_id, visit_date DESC) as row
+  FROM visits  
+  ),
+
+next_appt AS (
+  SELECT patient_id,
+         id AS appt_id,
+         date AS appt_date,
+         ROW_NUMBER() OVER (PARTIION BY patient_id, visit_date ASC) as row
+  FROM appointments
+  )
+  
+--all pediatric pts, does not matter if they had a recent visit
+SELECT
+  patients.dob AS date_of_birth,
+FROM patients
+LEFT JOIN last_visit ON visits.patient_id = patients.id AND last_visit.row = 1
+LEFT JOIN last_appt ON last_appt.patient_id = patients.id AND last_appt.row = 1
+WHERE EXTRACT(YEAR FROM AGE(dob)) < 18
